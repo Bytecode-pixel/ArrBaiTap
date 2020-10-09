d33b29a3732b72ac44ee515c553158f121b61172
Bytecode-pixel <ducnhan199x@gmail.com>
2020-10-09 20:59:50 +0700

    ArrBaiTap.git

---
+++ js/index.js
@@ -0,0 +1,18 @@
+//b1
+var NumArr = [1,2,3,4,5,6,7,8,9,9,8,7,6,5,4,3,2,1];
+
+var SumArr = NumArr.reduce((SumN,value) => SumN + value);
+
+console.log(SumArr);
+console.log("Bài 1: " ,SumArr);
+
+//b2
+var maxInNumbers = Math.max.apply(Math, NumArr); 
+var minInNumbers = Math.min.apply(Math, NumArr);
+var MediumNumbers = Math.floor( NumArr.reduce((a,b)=> a+b)/NumArr.length);
+
+console.log(" Bài 2a: " ,maxInNumbers);
+console.log(" Bài 2b: " ,minInNumbers);
+console.log(" Bài 2c: " ,MediumNumbers);
+
+//b3
